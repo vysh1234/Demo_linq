3d5141a535555cf2a16857a9e1d88f84ad7cb6a4
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 11:53:43 +0530

    linq

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace Demo_linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linq implementation");
+
+           // int[] scores = new int[] { 40, 33, 77, 54, 22 };
+
+            string[] fruits = new string[] { "banana", "apple", "Pineapple", "Mango", "watermelon" };
+
+            //IEnumerable<int> ScoreQuery =
+              //  from score in scores
+                //where score is > 30
+                //select score;
+            IEnumerable<string> FruitQuery =
+                from fruit in fruits
+                where fruit is "apple" or "banana"
+                select fruit;
+
+            foreach(var item in FruitQuery)
+            {
+                Console.WriteLine(item+" \n");
+            }
+        }
+    }
+}
